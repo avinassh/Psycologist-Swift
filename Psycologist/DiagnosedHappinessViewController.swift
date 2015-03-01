85f798104bf30390c6ab0b5daba139a584d8ebd5
avi <avinashsajjan@gmail.com>
2015-03-01 15:52:57 +0530

    created subclass DiagnosedHappinessViewController, which will handle diagnostic details.

---
+++ Psycologist/DiagnosedHappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DiagnosedHappinessViewController.swift
+//  Psycologist
+//
+//  Created by avi on 01/03/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import UIKit
+
+class DiagnosedHappinessViewController: HappinessViewController {
+    
+}

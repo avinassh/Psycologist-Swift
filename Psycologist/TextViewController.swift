c631ec5dc30abf675147bc3b1d7f44badeba445d
avi <avinashsajjan@gmail.com>
2015-03-01 15:48:42 +0530

    added simple text view and controller for popover display

---
+++ Psycologist/TextViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TextViewController.swift
+//  Psycologist
+//
+//  Created by avi on 01/03/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            textView.text = text
+        }
+    }
+    
+    var text: String = "" {
+        didSet {
+            textView?.text = text
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

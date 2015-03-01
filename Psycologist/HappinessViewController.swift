47f4ff527cfc0e850e26fd8157857440b9583608
avi <avinashsajjan@gmail.com>
2015-03-01 14:05:23 +0530

    added files and storyboard scenes from happiness view controller.

---
+++ Psycologist/HappinessViewController.swift
@@ -0,0 +1,78 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by avi on 28/02/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource {
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+            // following is perfectly fine. however we will add pan gesture via storyboard
+            // faceView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "changeHappiness:"))
+        }
+    }
+    
+    var happiness: Int = 30 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            println("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let happinessGestureScale: CGFloat = 4
+    }
+    
+    private func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+            case .Ended: fallthrough
+            case .Changed:
+                let translation = gesture.translationInView(faceView)
+                let happinessChange = -Int(translation.y/Constants.happinessGestureScale)
+                if happinessChange != 0 {
+                    happiness += happinessChange
+                    gesture.setTranslation(CGPointZero, inView: faceView)
+                }
+            default: break
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
